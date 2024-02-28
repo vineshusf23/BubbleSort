d5f3dd753be12152b4883dc565e54e667786ed45
Vinesh Narendra Sangoi <sangoiv@usf.edu>
2024-02-28 11:01:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Bubblesort
+{
+    static void Main()
+    {
+        int[] arr = { 5, 72, 3, 14, 7, 1 };
+
+        Console.WriteLine("Original Array: " + string.Join(", ", arr));
+
+        BubbleSort(arr);
+
+        Console.WriteLine("Sorted Array: " + string.Join(", ", arr));
+    }
+
+    static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // Swap elements if they are in the wrong order
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
